5b10ffaa0483e4b881e3f02fdce527c5c978ba4d
tayyab94 <tayyab.bhatti30@gmail.com>
2022-01-23 20:59:59 +0500

    Impletem Dotnet Core APi with Angular

---
+++ ImpectionAPIApp/ImpectionAPI/Models/Inspection.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImpectionAPI.Models
+{
+    public class Inspection
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Stauts { get; set; } = string.Empty;
+
+
+        [StringLength(200)]
+        public string Comment { get; set; } = string.Empty;
+
+        public int InspectionId { get; set; }
+
+        public InspectionType? InspectionType { get; set; }
+    }
+}
